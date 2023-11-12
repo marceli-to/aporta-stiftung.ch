3f97a19ca08d9a28f71a1f3f51b9aaf5b511b1f2
Marcel Stadelmann <m@marceli.to>
2023-11-12 14:07:21 +0100

    wip

---
+++ config/client.php
@@ -0,0 +1,5 @@
+<?php
+return [
+  'base_url' => env('APP_URL'),
+  'meta_description' => 'Die Dr. Stephan à Porta Stiftung unterstützt mit dem Überschuss aus der Liegenschaftsbewirtschaftung wohltätige und gemeinnützige Institutionen.'
+];
