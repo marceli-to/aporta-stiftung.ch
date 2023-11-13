9ba852f9113286b8c23cd00505598f4c5a010daf
Marcel Stadelmann <m@marceli.to>
2023-11-13 17:40:46 +0100

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,10 +25,16 @@ export default {
       },
 
       fontSize: {
-        'sm': ['0.9375rem', '1.33333'], // 15px
-        'base': ['1rem', '1.375'],        // 16px
-        'md': ['1.125rem', '1.33333'],  // 18px
-        'lg': ['1.25rem', '1.33333'],   // 20px
+        'xxs': '0.75rem',   // 12px
+        'xs': '0.875rem',   // 14px
+        'sm': '1.0rem',     // 16px 
+        'md': '1.125rem',   // 18px
+        'lg': '1.25rem',    // 20px
+        'xl': '1.5rem',     // 24px
+        '2xl': '1.875rem',  // 30px
+        '3xl': '2.0rem',    // 32px
+        '4xl': '2.25rem',   // 36px
+        '5xl': '2.5rem',    // 40px
       },
 
       colors: {
