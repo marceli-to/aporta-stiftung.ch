4392f4d863e9a07d490c6a03fa84417261dbd692
Marcel Stadelmann <m@marceli.to>
2023-12-22 14:31:41 +0100

    wip

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -10,7 +10,7 @@ class Kernel extends ConsoleKernel
    */
   protected function schedule(Schedule $schedule): void
   {
-    $schedule->call('submit:application')->everyMinute();
+    $schedule->command('submit:application')->everyMinute();
   }
 
   /**
