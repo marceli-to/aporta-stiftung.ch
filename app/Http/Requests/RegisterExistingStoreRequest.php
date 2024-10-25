17a4ddfc0d75ff84e2fb27283ae13e41aa3a5d2f
Marcel Stadelmann <m@marceli.to>
2024-10-25 10:45:52 +0200

    wip

--- app/Http/Requests/RegisterExistingStoreRequest.php
+++ app/Http/Requests/RegisterExistingStoreRequest.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\Rule;
 
 class RegisterExistingStoreRequest extends FormRequest
 {
